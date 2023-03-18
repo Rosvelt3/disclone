0eba099ed22f17de73794f3f07437e82ab80667c
isaias005 <isaias005@hotmail.com>
2023-03-17 22:40:31 -0400

    commented out

--- src/app/join/[serverId]/JoinServerCard.tsx
+++ src/app/join/[serverId]/JoinServerCard.tsx
@@ -1,56 +1,56 @@
 "use client";
 
-import { Server } from "@/app/ServerList";
-import pb from "@/lib/appwrite";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
+// import { Server } from "@/app/ServerList";
+// import pb from "@/lib/appwrite";
+// import { useMutation, useQuery } from "@tanstack/react-query";
+// import { useRouter } from "next/navigation";
 
 export default function JoinServerCard({ serverId }: { serverId: string }) {
-  const router = useRouter();
+//   const router = useRouter();
 
-  const { data: server } = useQuery({
-    queryKey: ["server", serverId],
-    queryFn: async () => {
-      const server = await pb.collection("servers").getOne<Server>(serverId);
-      return server;
-    },
-  });
+//   const { data: server } = useQuery({
+//     queryKey: ["server", serverId],
+//     queryFn: async () => {
+//       const server = await pb.collection("servers").getOne<Server>(serverId);
+//       return server;
+//     },
+//   });
 
-  const useJoinServerMutation = () =>
-    useMutation({
-      mutationFn: async () => {
-        if (!server) return;
-        const userId = await pb.authStore.model?.id;
-        await pb.collection("servers").update(serverId, {
-          users: [...server.users, userId],
-        });
-      },
-      onSuccess: () => {
-        router.push(`/servers/${serverId}`);
-      },
-    });
+//   const useJoinServerMutation = () =>
+//     useMutation({
+//       mutationFn: async () => {
+//         if (!server) return;
+//         const userId = await pb.authStore.model?.id;
+//         await pb.collection("servers").update(serverId, {
+//           users: [...server.users, userId],
+//         });
+//       },
+//       onSuccess: () => {
+//         router.push(`/servers/${serverId}`);
+//       },
+//     });
 
-  const joinServerMutation = useJoinServerMutation();
-
-  return (
-    <div className="flex h-full items-center justify-center bg-gradient-to-br from-sky-600 to-blue-800">
-      <div className="w-1/2 max-w-lg rounded-sm bg-slate-700 p-8 shadow-sm">
-        <h1 className="mb-6 text-center text-2xl text-white">
-          Join the {server?.name} server
-        </h1>
-        <div className="mx-auto flex h-[4rem] w-[4rem] items-center justify-center rounded-lg bg-slate-600">
-          <div className="flex cursor-pointer items-center justify-center rounded-lg text-4xl text-white transition-all hover:rounded-none">
-            {server?.name[0].toUpperCase()}
-          </div>
-        </div>
-        <button
-          onClick={() => joinServerMutation.mutate()}
-          type="button"
-          className="mx-auto mt-6 mb-4 block w-32 rounded-md bg-sky-500 p-2 font-medium text-white transition-colors hover:bg-sky-600"
-        >
-          Join
-        </button>
-      </div>
-    </div>
-  );
+//   const joinServerMutation = useJoinServerMutation();
+return <div></div>
+//   return (
+//     <div className="flex h-full items-center justify-center bg-gradient-to-br from-sky-600 to-blue-800">
+//       <div className="w-1/2 max-w-lg rounded-sm bg-slate-700 p-8 shadow-sm">
+//         <h1 className="mb-6 text-center text-2xl text-white">
+//           Join the {server?.name} server
+//         </h1>
+//         <div className="mx-auto flex h-[4rem] w-[4rem] items-center justify-center rounded-lg bg-slate-600">
+//           <div className="flex cursor-pointer items-center justify-center rounded-lg text-4xl text-white transition-all hover:rounded-none">
+//             {server?.name[0].toUpperCase()}
+//           </div>
+//         </div>
+//         <button
+//           onClick={() => joinServerMutation.mutate()}
+//           type="button"
+//           className="mx-auto mt-6 mb-4 block w-32 rounded-md bg-sky-500 p-2 font-medium text-white transition-colors hover:bg-sky-600"
+//         >
+//           Join
+//         </button>
+//       </div>
+//     </div>
+//   );
 }
--- src/app/servers/[serverId]/ServerUsers.tsx
+++ src/app/servers/[serverId]/ServerUsers.tsx
@@ -1,81 +1,82 @@
 "use client";
 
-import pb from "@/lib/appwrite";
-import { useQuery } from "@tanstack/react-query";
-import Image from "next/image";
-import { Record } from "pocketbase";
+// import pb from "@/lib/appwrite";
+// import { useQuery } from "@tanstack/react-query";
+// import Image from "next/image";
+// import { Record } from "pocketbase";
 
-export type User = { name: string; avatar: string; owner: boolean } & Record;
-type ServerUsersProps = { serverId: string };
+// export type User = { name: string; avatar: string; owner: boolean } & Record;
+// type ServerUsersProps = { serverId: string };
 
-export default function ServerUsers({ serverId }: ServerUsersProps) {
-  const { data: users } = useQuery({
-    queryKey: ["listUsersInServer", serverId],
-    queryFn: async () => {
-      const server = await pb.collection("servers").getOne(serverId!, {
-        expand: "users",
-      });
+// export default function ServerUsers({ serverId }: ServerUsersProps) {
+//   const { data: users } = useQuery({
+//     queryKey: ["listUsersInServer", serverId],
+//     queryFn: async () => {
+//       const server = await pb.collection("servers").getOne(serverId!, {
+//         expand: "users",
+//       });
 
-      const serverUsers = server?.expand?.users as User[];
-      const newServerUsers = serverUsers?.map((user) => {
-        return {
-          ...user,
-          owner: user.id === server.owner,
-        };
-      });
+//       const serverUsers = server?.expand?.users as User[];
+//       const newServerUsers = serverUsers?.map((user) => {
+//         return {
+//           ...user,
+//           owner: user.id === server.owner,
+//         };
+//       });
 
-      return newServerUsers || [];
-    },
-    enabled: !!serverId,
-  });
+//       return newServerUsers || [];
+//     },
+//     enabled: !!serverId,
+//   });
 
-  if (users?.length === 0)
-    return (
-      <div className="items-left flex h-full w-48 flex-col gap-4 bg-slate-800 p-4 text-white">
-        There are no users in this server
-      </div>
-    );
-  return (
-    <div className="items-left flex h-full w-48 flex-col gap-4 bg-slate-800 p-4">
-      <h2 className="text-white">Owner</h2>
-      {users?.map(({ name, id, avatar, owner, collectionName }) => {
-        if (!owner) return null;
-        const userAvatarUrl = avatar
-          ? `${process.env.NEXT_PUBLIC_POCKETBASE_URL}/api/files/${collectionName}/${id}/${avatar}`
-          : "/defaultAvatar.webp";
-        return (
-          <div className="flex items-center gap-4" key={id}>
-            <Image
-              src={userAvatarUrl}
-              alt=""
-              width={40}
-              height={40}
-              className="rounded-full"
-            />
-            <span className="text-lg text-white">{name}</span>
-          </div>
-        );
-      })}
+return <div></div>
+//   if (users?.length === 0)
+//     return (
+//       <div className="items-left flex h-full w-48 flex-col gap-4 bg-slate-800 p-4 text-white">
+//         There are no users in this server
+//       </div>
+//     );
+//   return (
+//     <div className="items-left flex h-full w-48 flex-col gap-4 bg-slate-800 p-4">
+//       <h2 className="text-white">Owner</h2>
+//       {users?.map(({ name, id, avatar, owner, collectionName }) => {
+//         if (!owner) return null;
+//         const userAvatarUrl = avatar
+//           ? `${process.env.NEXT_PUBLIC_POCKETBASE_URL}/api/files/${collectionName}/${id}/${avatar}`
+//           : "/defaultAvatar.webp";
+//         return (
+//           <div className="flex items-center gap-4" key={id}>
+//             <Image
+//               src={userAvatarUrl}
+//               alt=""
+//               width={40}
+//               height={40}
+//               className="rounded-full"
+//             />
+//             <span className="text-lg text-white">{name}</span>
+//           </div>
+//         );
+//       })}
 
-      {users && users?.length > 1 && <h2 className="text-white">Users</h2>}
-      {users?.map(({ name, id, avatar, owner, collectionName }) => {
-        if (owner) return null;
-        const userAvatarUrl = avatar
-          ? `${process.env.NEXT_PUBLIC_POCKETBASE_URL}/api/files/${collectionName}/${id}/${avatar}`
-          : "/defaultAvatar.webp";
-        return (
-          <div className="flex items-center gap-4" key={id}>
-            <Image
-              src={userAvatarUrl}
-              alt=""
-              width={40}
-              height={40}
-              className="rounded-full"
-            />
-            <span className="text-lg text-white">{name}</span>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+//       {users && users?.length > 1 && <h2 className="text-white">Users</h2>}
+//       {users?.map(({ name, id, avatar, owner, collectionName }) => {
+//         if (owner) return null;
+//         const userAvatarUrl = avatar
+//           ? `${process.env.NEXT_PUBLIC_POCKETBASE_URL}/api/files/${collectionName}/${id}/${avatar}`
+//           : "/defaultAvatar.webp";
+//         return (
+//           <div className="flex items-center gap-4" key={id}>
+//             <Image
+//               src={userAvatarUrl}
+//               alt=""
+//               width={40}
+//               height={40}
+//               className="rounded-full"
+//             />
+//             <span className="text-lg text-white">{name}</span>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
